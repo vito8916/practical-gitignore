6abc2b5245d1efed1006619ce83cdb8a3450ce44
victor_sp <victor@spfairvalue.com>
2022-01-05 11:25:47 -0600

    change readme

--- config/db.php
+++ config/db.php
@@ -10,5 +10,5 @@ return [
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
-    'schemaCache' => 'cache',
+    //'schemaCache' => 'cache',
 ];
